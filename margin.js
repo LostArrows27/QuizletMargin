f455303854c0a947bd89cc5c624ebe92fa5b9a7d
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-09-29 09:40:34 +0700

    Fixing bug after re-learning flashcard

--- margin.js
+++ margin.js
@@ -1,4 +1,27 @@
-var FlashCard = document.querySelector('.oteer4c')
-var blockedElement = document.querySelector('.a1ijmfr8')
-blockedElement.remove();
-FlashCard.style.setProperty('--oteer4c-1', 'center')
+let FlashCard = document.querySelector('.oteer4c')
+let blockedElement = document.querySelector('.a1ijmfr8')
+
+if (FlashCard != null) {
+    FlashCard.style.setProperty('--oteer4c-1', 'center')
+}
+
+if (blockedElement) {
+    blockedElement.style.margin = "0";
+    blockedElement.style.padding = "0";
+    blockedElement.style.flex = "0";
+}
+
+document.addEventListener("click", () => {
+    let FlashCard = document.querySelector('.oteer4c')
+    let blockedElement = document.querySelector('.a1ijmfr8')
+
+    if (FlashCard != null) {
+        FlashCard.style.setProperty('--oteer4c-1', 'center')
+    }
+
+    if (blockedElement) {
+        blockedElement.style.margin = "0";
+        blockedElement.style.padding = "0";
+        blockedElement.style.flex = "0";
+    }
+})
