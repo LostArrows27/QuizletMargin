eb9208ad930250e0ffb72df0791d7f048042c69b
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-09-29 08:35:03 +0700

    Initial commit

---
+++ margin.js
@@ -0,0 +1,4 @@
+var FlashCard = document.querySelector('.oteer4c')
+var blockedElement = document.querySelector('.a1ijmfr8')
+blockedElement.remove();
+FlashCard.style.setProperty('--oteer4c-1', 'center')
